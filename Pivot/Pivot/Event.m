ed3de382cde8bd33d4cb319ec1382935361f1721
JovannyEspinal <espinaljovanny@icloud.com>
2015-10-31 12:55:07 -0400

    Added Event class.

---
+++ Pivot/Pivot/Event.m
@@ -0,0 +1,14 @@
+//
+//  Event.m
+//  Pivot
+//
+//  Created by Jovanny Espinal on 10/31/15.
+//  Copyright © 2015 Jason Wang. All rights reserved.
+//
+
+#import "Event.h"
+
+@implementation Event
+
+
+@end

b8fc365fc49e054c48111e9984893390cd470021
Jason Wang <qwang216@gmail.com>
2015-10-31 12:58:07 -0400

    added education model

---
+++ Pivot/Pivot/Education.h
@@ -0,0 +1,17 @@
+//
+//  Education.h
+//  Pivot
+//
+//  Created by Jason Wang on 10/31/15.
+//  Copyright © 2015 Jason Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Education : NSObject
+
+@property (nonatomic) NSString *school;
+@property (nonatomic) NSString *degreeType;
+@property (nonatomic) NSString *major;
+
+@end
---
+++ Pivot/Pivot/Education.m
@@ -0,0 +1,13 @@
+//
+//  Education.m
+//  Pivot
+//
+//  Created by Jason Wang on 10/31/15.
+//  Copyright © 2015 Jason Wang. All rights reserved.
+//
+
+#import "Education.h"
+
+@implementation Education
+
+@end

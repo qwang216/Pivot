0a858fa84ea567dd665803dd13e29f5ef235cbdc
Lauren <laurencaponong@gmail.com>
2015-11-01 13:04:58 -0500

    hooked up buttons on add experience

--- Pivot/Pivot/AddExperienceViewController.m
+++ Pivot/Pivot/AddExperienceViewController.m
@@ -9,6 +9,9 @@
 #import "AddExperienceViewController.h"
 
 @interface AddExperienceViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *yearTextField;
+@property (weak, nonatomic) IBOutlet UITextField *headlineTextField;
+@property (weak, nonatomic) IBOutlet UITextView *detailTextView;
 
 @end
 
@@ -17,6 +20,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+- (IBAction)switchForPivotPoint:(id)sender {
+}
+
+- (IBAction)saveButtonTapped:(id)sender {
 }
 
 - (void)didReceiveMemoryWarning {
